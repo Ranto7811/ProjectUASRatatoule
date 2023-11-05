0ae8f97fe71a6434ba0f862e7f05e0f33b475bd1
Ranto7811 <rantoo041203@gamil.com>
2023-11-05 14:38:20 +0700

    Memperbaiki registerActivity dengan intent ke login activity

--- app/src/main/java/com/example/projectuas/LoginActivity.kt
+++ app/src/main/java/com/example/projectuas/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
@@ -15,19 +16,26 @@ class LoginActivity : AppCompatActivity() {
         val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
         val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
         val loginButton = findViewById<Button>(R.id.loginButton)
+        val noAccount = findViewById<TextView>(R.id.NoAccountTextView)
 
         loginButton.setOnClickListener{
             val username = usernameEditText.text.toString()
             val password = passwordEditText.text.toString()
 
             if (username == "ranto" && password == "ranto") {
-                val intent = Intent(this, RegisterActivity::class.java)
-                startActivity(intent)
+                val pesan = "Berhasil login"
+                showToast(pesan)
             } else {
                 val pesan = "Login gagal"
                 showToast(pesan)
             }
         }
+
+        noAccount.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/projectuas/RegisterActivity.kt
+++ app/src/main/java/com/example/projectuas/RegisterActivity.kt
@@ -1,11 +1,34 @@
 package com.example.projectuas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val emailEditText = findViewById<EditText>(R.id.EmailEditText)
+        val registerButton = findViewById<Button>(R.id.RegisterButton)
+
+        registerButton.setOnClickListener{
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            val email = emailEditText.text.toString()
+
+            if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                Toast.makeText(this, "Harap isi semua kolom", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Berhasil mendaftar akun", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
