5d1f94696831b7329c818ef21d5ebd06bdf51aff
Ranto7811 <rantoo041203@gamil.com>
2023-11-05 14:07:25 +0700

    Memperbaiki login activity

--- app/src/main/java/com/example/projectuas/LoginActivity.kt
+++ app/src/main/java/com/example/projectuas/LoginActivity.kt
@@ -1,11 +1,35 @@
 package com.example.projectuas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener{
+            val username = usernameEditText.text.toString()
+            val password = usernameEditText.text.toString()
+
+            if (username == "Ranto" && password == "contohpassword") {
+                val intent = Intent(this, RegisterActivity::class.java)
+                startActivity(intent)
+            } else {
+                val pesan = "Login gagal"
+                showToast(pesan)
+            }
+        }
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
