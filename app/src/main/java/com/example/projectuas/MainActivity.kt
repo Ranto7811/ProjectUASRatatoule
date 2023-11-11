4a0e1af44882bbbedfff3b4937a074df07497dfd
asterracea <alifwidya25@gmail.com>
2023-11-11 23:06:13 +0700

    menambahkan recycler view pada main activity

--- app/src/main/java/com/example/projectuas/MainActivity.kt
+++ app/src/main/java/com/example/projectuas/MainActivity.kt
@@ -2,12 +2,37 @@ package com.example.projectuas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    //recyclerview
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var makananAdapter: FoodListAdapter
+    private val foodList = listOf(
+        Makanan("Cireng", R.drawable.cireng),
+        Makanan("Perkedel", R.drawable.perkedel),
+        Makanan("Orak-Arik Tempe", R.drawable.tmpe),
+        Makanan("Omellet", R.drawable.cireng),
+        Makanan("Ayam Suwir", R.drawable.ayamsuwi)
+        // Tambahkan lebih banyak makanan jika diperlukan
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // firebase
+
+
         //menghapus title project pada bagian atas
         supportActionBar?.hide()
+
+        //recycleview
+        recyclerView = findViewById(R.id.rv)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        makananAdapter = FoodListAdapter(foodList)
+        recyclerView.adapter = makananAdapter
     }
 }
