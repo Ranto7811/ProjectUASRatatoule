0d1a6f8e51bb3d1e1880f969a2c56f0de350e40d
asterracea <alifwidya25@gmail.com>
2023-12-16 18:46:03 +0700

    update adapter menggunakan firebase

--- app/src/main/java/com/example/projectuas/FoodListAdapter.kt
+++ app/src/main/java/com/example/projectuas/FoodListAdapter.kt
@@ -1,20 +1,26 @@
 package com.example.projectuas
 
-import android.content.Context
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.projectuas.model.Masakan
+import com.bumptech.glide.Glide
 
 class FoodListAdapter(private val foodList: List<Makanan>) :
     RecyclerView.Adapter<FoodListAdapter.FoodViewHolder>() {
     class FoodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val TitleResep: TextView = itemView.findViewById(R.id.tv_food_home)
         val imageResep: ImageView = itemView.findViewById(R.id.iv_food_home)
+        fun bind(resep: Makanan) {
+            TitleResep.text = resep.judul
+            // Gunakan Glide untuk memuat dan menampilkan gambar
+            Glide.with(itemView.context)
+                .load(resep.image)
+                .placeholder(R.drawable.cireng) // placeholder gambar sementara
+                .into(imageResep)
+        }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.activity_list_home, parent, false)
@@ -23,7 +29,7 @@ class FoodListAdapter(private val foodList: List<Makanan>) :
 
     override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
         val makanan = foodList[position]
-        holder.TitleResep.text = makanan.resep
+        holder.bind(makanan)
 
     }
 
